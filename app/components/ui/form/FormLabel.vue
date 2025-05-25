4c4f122167ad66aad1b358f7b6bda49581972a83
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2025-05-25 11:54:17 +0400

    feat: adding form component

---
+++ app/components/ui/form/FormLabel.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import type { LabelProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import { cn } from '~/lib/utils'
+import { useFormField } from './useFormField'
+
+const props = defineProps<LabelProps & { class?: HTMLAttributes['class'] }>()
+
+const { error, formItemId } = useFormField()
+</script>
+
+<template>
+  <UiLabel
+    data-slot="form-label"
+    :data-error="!!error"
+    :class="cn(
+      'data-[error=true]:text-destructive-foreground',
+      props.class,
+    )"
+    :for="formItemId"
+  >
+    <slot />
+  </UiLabel>
+</template>

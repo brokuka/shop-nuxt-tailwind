11fe150dff5800c05213ffb33531adb19a1e0e72
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2025-05-25 11:56:25 +0400

    chore: update dialog components

--- app/components/ui/dialog/DialogClose.vue
+++ app/components/ui/dialog/DialogClose.vue
@@ -6,10 +6,9 @@ const props = defineProps<DialogCloseProps>()
 </script>
 
 <template>
-  <DialogClose
-    data-slot="dialog-close"
-    v-bind="props"
-  >
-    <slot />
-  </DialogClose>
+  <UiButton size="icon" variant="ghost" class="top-4 right-4 [&_svg:not([class*='size-'])]:size-5 absolute" as-child>
+    <DialogClose data-slot="dialog-close" v-bind="props">
+      <slot />
+    </DialogClose>
+  </UiButton>
 </template>

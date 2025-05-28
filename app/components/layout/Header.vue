e32891efb3cd3279d75b9a6aea1ada6d327d3dc6
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2025-05-28 15:28:44 +0400

    chore(layout-component): update header

--- app/components/layout/Header.vue
+++ app/components/layout/Header.vue
@@ -1,7 +1,22 @@
 <script setup lang="ts">
+import type { SignInValues } from '~/lib/types'
 import { User } from 'lucide-vue-next'
 
 const isDialogOpened = ref(false)
+
+const authStore = useAuthStore()
+
+async function signIn(values: SignInValues, isLoading: Ref<boolean>) {
+  isLoading.value = true
+
+  await authStore.signIn('email', values)
+
+  isLoading.value = false
+}
+
+async function sendOtp(email: string) {
+  await authStore.sendOtp(email)
+}
 </script>
 
 <template>
@@ -22,24 +37,28 @@ const isDialogOpened = ref(false)
     </div>
 
     <div class="justify-end flex gap-2 col-span-2">
-      <UiTooltipProvider>
+      <UiTooltipProvider v-if="!authStore.user">
         <UiTooltip>
           <UiTooltipTrigger as-child>
             <UiButton size="icon" variant="outline" @click="isDialogOpened = !isDialogOpened">
               <User />
             </UiButton>
-          </UiTooltipTrigger>
 
-          <UiTooltipContent>
-            <p>Вход</p>
-          </UiTooltipContent>
+            <UiTooltipContent>
+              <p>
+                Вход
+              </p>
+            </UiTooltipContent>
+          </uitooltiptrigger>
         </UiTooltip>
       </UiTooltipProvider>
+
+      <BlockHeaderMiniProfile v-else v-bind="authStore.user" />
 
       <!-- <UiButton>Button2</UiButton>
       <UiButton>Button3</UiButton> -->
     </div>
   </header>
 
-  <BlockAuthSignBase v-model:open="isDialogOpened" />
+  <BlockAuthSignBase v-if="!authStore.user" v-model:open="isDialogOpened" @sign-in="signIn" @send-otp="sendOtp" />
 </template>

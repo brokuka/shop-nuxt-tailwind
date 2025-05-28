36bf2b4c3b6990f3e3192b7964e0a9dba21cbd65
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2025-05-28 15:22:15 +0400

    feat: adding common types

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -1,14 +1,6 @@
-import * as z from 'zod'
-import { PIN_SLOT_LENGTH } from './constants'
+export type AcceptedAuthMethods = 'github' | 'vk' | 'email'
 
-export const emailSchema = z.object({
-  email: z.string().min(2).max(50).email(),
-})
-
-export type EmailSchemaType = z.infer<typeof emailSchema>
-
-export const pinSchema = z.object({
-  pin: z.array(z.string({ required_error: 'Код должен быть полностью введён' })).length(PIN_SLOT_LENGTH, { message: 'Код должен быть полностью введён' }),
-})
-
-export type PinSchemaType = z.infer<typeof pinSchema>
+export interface SignInValues {
+  email: string
+  otp: string
+}

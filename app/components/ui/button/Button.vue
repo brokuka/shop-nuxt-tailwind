eaa490b8591d3a3d7130dea4a26833b6f7d36dcc
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2025-05-28 14:44:33 +0400

    feat(button): adding loading state

--- app/components/ui/button/Button.vue
+++ app/components/ui/button/Button.vue
@@ -2,6 +2,7 @@
 import type { PrimitiveProps } from 'reka-ui'
 import type { HTMLAttributes } from 'vue'
 import type { ButtonVariants } from '.'
+import { Loader2 } from 'lucide-vue-next'
 import { Primitive } from 'reka-ui'
 import { cn } from '~/lib/utils'
 import { buttonVariants } from '.'
@@ -10,6 +11,8 @@ interface Props extends PrimitiveProps {
   variant?: ButtonVariants['variant']
   size?: ButtonVariants['size']
   class?: HTMLAttributes['class']
+  disabled?: boolean
+  loading?: boolean
 }
 
 const props = withDefaults(defineProps<Props>(), {
@@ -19,11 +22,11 @@ const props = withDefaults(defineProps<Props>(), {
 
 <template>
   <Primitive
-    data-slot="button"
-    :as="as"
-    :as-child="asChild"
+    data-slot="button" :as="as" :as-child="asChild"
+    :disabled="disabled || loading"
     :class="cn(buttonVariants({ variant, size }), props.class)"
   >
+    <Loader2 v-if="loading" class="w-4 h-4 animate-spin" />
     <slot />
   </Primitive>
 </template>
